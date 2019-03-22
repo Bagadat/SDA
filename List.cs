8be2e26cf039150b8173f67d9f47ce9b7fdfa415
Bagadat <32618276+Bagadat@users.noreply.github.com>
2019-03-22 12:57:14 +0300

    Add files via upload

---
+++ List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class DoubleK
+    {
+        public int data { get; set; }
+        public DoubleK (int data)
+        {
+           this.data = data;
+        }
+
+        public DoubleK Next { get; set; }
+        public DoubleK Previous { get; set; } 
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedlist = new LinkedList();
+
+            linkedlist.AddFirst(1);
+            linkedlist.AddLast(2);
+            
+            for (int i = 3;i < 11; i++)
+            {
+                linkedlist.AddLast(i);
+            }
+            linkedlist.AddLast(55);
+
+            linkedlist.Remove(3);
+            Console.WriteLine(linkedlist.Contains(3));
+           
+
+            Console.ReadKey();
+        }
+    }
+}
